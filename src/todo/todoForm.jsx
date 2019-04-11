3f6825976884ca1ff6061ffa14812018625de402
Paulo Márcio <paulom.matias@gmail.com>
2019-04-11 06:53:31 -0300

    [enh] Done redux integration for TodoForm and TodoList

--- src/todo/todoForm.jsx
+++ src/todo/todoForm.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux';
 
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription } from './todoActions'
 
-export default props => {
+const TodoForm = props => {
     const keyHandler = (e) => {
         if(e.key === 'Enter'){
             e.shiftKey ? props.handleSearch() : props.handleAdd()
@@ -19,7 +22,7 @@ export default props => {
                     id='description'
                     className='form-control'
                     placeholder='Adicione uma tarefa'
-                    onChange={props.handleChange}
+                    onChange={props.changeDescription}
                     onKeyUp={keyHandler}
                     value={props.description}/>
             </Grid>
@@ -34,3 +37,8 @@ export default props => {
         </div>
     )
 }
+
+const mapStateToProps = state => ({description: state.todo.description})
+const mapDispatchToProps = dispatch => bindActionCreators({ changeDescription }, dispatch)
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
